22e4f6d0c2c664b4091155cf3f0f95c3db832835
Balos87 <145317796+Balos87@users.noreply.github.com>
2023-11-16 08:26:02 +0100

    Add project files.

---
+++ DemoRealTimeSystemSimulator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoRealTimeSystemSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Real-Time system simulator started.");
+            Console.WriteLine("Type 'exit' to stop the program.");
+
+            // Creating and starting the clock thread.
+            Thread clockThread = new Thread(new ThreadStart(Clock));
+            clockThread.Start();
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "exit")
+                {
+                    Console.WriteLine("Exiting program.");
+                    break;
+                }
+                // Additional commands can be handled here
+
+            }
+            // Joining the clockthread before exiting
+            clockThread.Join();
+        }
+        static void Clock()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Current Time: {DateTime.Now.ToLongTimeString()}");
+                Thread.Sleep(1000); // Sleeps for 1second
+            }
+        }
+    }
+}
