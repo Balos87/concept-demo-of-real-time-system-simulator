33cf8186041675c9e20464285b20ce09069e237a
Balos87 <145317796+Balos87@users.noreply.github.com>
2023-11-16 12:20:52 +0100

    Update to project
    Added notes and commands.

--- DemoRealTimeSystemSimulator/Program.cs
+++ DemoRealTimeSystemSimulator/Program.cs
@@ -11,9 +11,13 @@ namespace DemoRealTimeSystemSimulator
             Console.WriteLine("Type 'exit' to stop the program.");
 
             // Creating and starting the clock thread.
+            // The thread is instanciated the same way as a class.
             Thread clockThread = new Thread(new ThreadStart(Clock));
+            // Starts the Thread
             clockThread.Start();
 
+            // A while loop that lets the user type what ever they want,
+            // untill they tipe 'exit'.
             while (true)
             {
                 string userInput = Console.ReadLine();
@@ -22,12 +26,33 @@ namespace DemoRealTimeSystemSimulator
                     Console.WriteLine("Exiting program.");
                     break;
                 }
+                else if (userInput.ToLower() == "clear")
+                {
+                    Console.Clear();
+                    Console.WriteLine("The screen has been cleared.");
+                }
+                else if (userInput.ToLower() == "check system")
+                {
+                    Console.WriteLine("The system is up and running");
+                }
+                else if (userInput.ToLower() == "commands")
+                {
+                    Console.WriteLine("Commands:\n\tTo exit: 'exit'\n\tTo clear prompt: clear\n\tTo system check: check system");
+                }
+                else
+                {
+                    Console.WriteLine($"Command unknown: '{userInput}'. Please type commands to see a list of commands availble.");
+                }
                 // Additional commands can be handled here
 
             }
             // Joining the clockthread before exiting
+            //By joining, what happens: When the main thread exit, it wait untill next thread will run its course before shutting down.
             clockThread.Join();
         }
+        //Method for displaying the time. Its in a whileloop and will continue-forever.
+        //Simply because there is no end-function set.
+        //The Thread.Sleep makes the thread tick again after set parameter, in this case: 1sec.
         static void Clock()
         {
             while (true)
